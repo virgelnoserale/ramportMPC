d33c3f355968219504238e265063de3e7a6a9744
virgelnoserale <jhanhanes@gmail.com>
2023-08-21 00:43:21 +0800

    contact pa din

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,23 +2,34 @@ import React, {useRef}from 'react'
 import emailjs from '@emailjs/browser';
 import images from '../components/images';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const Contact = () => {
 
   const form = useRef();
-
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm('service_4sgwexp', 'template_qzz18xe', form.current, 'Jv0oZqwXuQH0IruZI')
       .then((result) => {
           console.log(result.text);
           console.log("sent");
+
+          
       }, (error) => {
           console.log(error.text);
           console.log("failed");
 
       });
+      
+
   };
+
+  const myAlert =() =>{
+    toast.success('Thank you');
+  }
+
 
   return (
     <div name="contact" style={{backgroundImage:`url(${images.contactImg})`,backgroundSize:'auto'}} className='h-screen'>
@@ -32,8 +43,21 @@ const Contact = () => {
       <input type="number"  name="cellphone" className='rounded-md p-2 mb-2' required placeholder='Phone Number'/>
       <input type="text"  name="country" className='rounded-md p-2 mb-2'required placeholder='Address' />
       <textarea name="message" rows={8} className='mb-4 rounded-md p-2 'required placeholder='Message'/>
-      <button type="submit" value="Send" className='text-white p-4 bg-black font-bold rounded-md '>Lets Talk</button>
+      <button onClick={myAlert} type="submit" value="Send" className='text-white p-4 bg-black font-bold rounded-md '>Lets Talk</button>
     </form>
+
+    <ToastContainer
+        position="top-left"
+        autoClose={false}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+       
+    
+     />
     </div>
     </div>
     </div>
